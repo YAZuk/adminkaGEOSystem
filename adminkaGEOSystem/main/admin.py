a8324789b11fce247942e92ecf5f0e4e5a97200e
Latypov Ramil <yazuk85@gmail.com>
2023-08-28 09:41:20 +0400

    new model company and device

--- adminkaGEOSystem/main/admin.py
+++ adminkaGEOSystem/main/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-from .models import Transport
+from .models import Transport, Company, Device
 
 
 # Register your models here.
 
 class AdminTransport(admin.ModelAdmin):
-    fields = ['name', 'vin', 'add_date']
-    list_display = ['name', 'vin', 'add_date']
+    fields = ['name', 'vin', 'company', 'device', 'add_date']
+    list_display = ['name', 'vin', 'company', 'device', 'add_date']
+    readonly_fields = ['add_date']
+
+
+class AdminCompany(admin.ModelAdmin):
+    fields = ['name', 'add_date']
+    list_display = ['name', 'add_date']
+    readonly_fields = ['add_date']
+
+
+class AdminDevice(admin.ModelAdmin):
+    fields = ['imei', 'name', 'company', 'device', 'add_date']
+    list_display = ['imei', 'name', 'add_date']
     readonly_fields = ['add_date']
 
 
 admin.site.register(Transport, AdminTransport)
+admin.site.register(Company, AdminCompany)
+admin.site.register(Device, AdminDevice)
--- adminkaGEOSystem/main/models.py
+++ adminkaGEOSystem/main/models.py
@@ -5,15 +5,42 @@ from django.db import models
 
 # Create your models here.
 
+
+class Device(models.Model):
+    id = models.AutoField(primary_key=True)
+    imei = models.CharField(max_length=1024, default="")
+    name = models.CharField(max_length=1024, default="")
+    add_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = u"Устройство"
+
+    def __str__(self):
+        return self.name + ' ' + self.imei
+
+
+class Company(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=1024, default="")
+    add_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = u"Компания"
+
+    def __str__(self):
+        return self.name
+
+
 class Transport(models.Model):
-    # id = models.IntegerField(primary_key=True, auto_created=True)
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=1024, default="")
     vin = models.CharField(max_length=1024, default="")
     add_date = models.DateTimeField(auto_now_add=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name_plural = "Транспорт"
+        verbose_name_plural = u"Транспорт"
 
     # created_by = models.ForeignKey('auth.User', default=get_current_user)
 
